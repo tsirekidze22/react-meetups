9ea8fdcde201768a4886abf850569c78a2d3a4f5
Tsotne Tsirekidze <tsotnetsirekidze22@gmail.com>
2022-10-09 18:59:56 +0400

    Layout renamed to layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Layout from "../components/Layout/Layout.js";
+import Layout from "../components/layout/Layout.js";
 
 function MyApp({ Component, pageProps }) {
   return (
