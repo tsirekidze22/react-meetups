bf51a9e7e1398255d319389986bb9f80158b46d3
Tsotne Tsirekidze <tsirekidze022@gmail.com>
2023-05-24 16:59:21 +0400

    minor changes

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -4,6 +4,7 @@ import classes from "./MeetupList.module.css";
 function MeetupList(props) {
   return (
     <ul className={classes.list}>
+      heelloo
       {props.meetups.map((meetup) => (
         <MeetupItem
           key={meetup.id}
